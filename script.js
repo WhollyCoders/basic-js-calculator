91abfb0ef8bd2f405ac0a015b4e48610f5c064b8
Michael L Parks <github@wmi-inc.com>
2019-07-16 19:40:55 -0400

    Add Boilerplate and Update Style

---
+++ script.js
@@ -0,0 +1,4 @@
+
+clearButton = document.getElementById('js-cal-btn-clear');
+oppositeButton = document.getElementById('js-cal-btn-opposite');
+percentButton = document.getElementById('js-cal-btn-percent');
